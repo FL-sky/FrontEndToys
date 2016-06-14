8deeb0089f935a23c0b98ebc071907a0bb90526a
Raynxxx <rayn1027@outlook.com>
2016-06-14 17:33:46 +0800

    start callbacks module

---
+++ jSelector/src/callbacks.js
@@ -0,0 +1,44 @@
+// Callbacks 模块
+const flagsCache = {};
+
+/**
+ * 将 string 式的 flags 转换为 object
+ */
+function createFlags (flags) {
+  let object = {};
+  flags = flags.split(/\s+/);
+  for (let i = 0, length = flags.length; i < length; ++i) {
+    object[flags[i]] = true;
+  }
+  flagsCache[flags] = object;
+  return object;
+}
+
+export default function Callbacks (flags) {
+  flags = flags ? (flagsCache[flags] || createFlags(flags)) : {};
+
+  // callback fn list
+  let list = [];
+
+  // add 1 or more callback fn
+  let addHelper = function (params) {
+
+  }
+
+  // trigger callback fn
+  let fireHelper = function (context, params) {
+    
+  }
+
+  let self = {
+    add: function () {
+      if (list) {
+        addHelper(arguments);
+      }
+    },
+    remove: function () {
+      
+    }
+  };
+  return self;
+}
--- jSelector/src/index.js
+++ jSelector/src/index.js
@@ -1,19 +1,66 @@
-(function (window, document, undefined) {
-  var w = window;
-  var doc = document;
+// 一切的入口，一个自调用匿名函数
+(function (window, undefined) {
+  let w = window;
+  let document = w.document;
 
-  var jSelector = function (selector, context) {
+  // 正则表达式
+  const rId = /^#[a-zA-Z0-9\-]+$/;
+
+  let jSelector = function (selector, context) {
     return new jSelector.prototype.init(selector, context);
   }
 
-  jSelector.prototype = {
+  jSelector.fn = jSelector.prototype = {
     constructor: jSelector,
+
+    // 选择器入口
     init: function (selector, context) {
+      // Handle $(""), $(null), $(undefined), $(false)
+      if (!selector) return this;
+
+      // handle $(DOMElement)
+      if (selector.nodeType) {
+        this.context = this[0] = selector[0];
+        this.length = 1;
+        return this;
+      }
+
+      // handle HTML Strings
+      if (typeof selector === 'string') {
+        this.context = context || document;
+        let _selector = selector.trim();
+        this.selector = _selector;
+        if (_selector.indexOf('#') === 0 && _selector.match(rId)) {
+          let elem = document.getElementById(_selector, this.context);
+          this[0] = elem;
+          this.length = 1;
+        } else {
+          let elems = this.context.querySelectorAll(selector);
+          for (let i = 0; i < elems.length; ++i) {
+            this[i] = elems[i];
+          }
+          this.length = elems.length;
+        }
+      }
+
+      // handle $($(..))
+      if (selector.selector !== undefined) {
+        this.selector = selector.selector;
+        this.context = selector.context;
+      }
       return this;
+    },
+
+    selector: '',
+    length: 0,
+
+    // 元素数量
+    size: function () {
+      return this.length;
     }
   }
 
-  jSelector.prototype.init.prototype = jSelector.prototype;
-
+  jSelector.fn.init.prototype = jSelector.fn;
+  
   w.jSelector = w.$ = jSelector;
-})(window, document);
+})(window);
